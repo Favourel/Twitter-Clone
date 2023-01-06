2944c26857421b89bcea1b08321687004fdd88d0
unknown <favourelodimuor15@gmail.com>
2023-01-06 20:08:42 +0100

    Push to server

--- TwitterClone/settings.py
+++ TwitterClone/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = "ki"
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
